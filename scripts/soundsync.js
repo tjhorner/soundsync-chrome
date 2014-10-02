c7d3d9bdae043edeba5af09dca1bfe3bd1339f36
TJ Horner <tj@swegbook.attlocal.net>
2014-10-01 20:29:39 -0700

    stuff

--- scripts/soundsync.js
+++ scripts/soundsync.js
@@ -4,8 +4,9 @@ Soundsync = (function(){
 	var socket = io.connect('soundsync.herokuapp.com'); // needed for https, thank you heroku.
 
 	$(document).ready(function(){
-		$li = $('<li><a href="chrome-extension://mmkmhkdddjclmodamjjjgbnfbkjcpkip/pages/options.html" target="_blank">Soundsync Settings</a></li>');
+		$li = $('<li><a href="'+chrome.extension.getURL("pages/options.html")+'" target="_blank">Soundsync Settings</a></li>');
 		$('.header__navMenu').last().append($li);
+		$controls = $('.playControls__playPauseSkip');
 	});
 
 	socket.on('hello', function(){
@@ -18,6 +19,18 @@ Soundsync = (function(){
 
 	socket.on('soundcloud:song:change', function(url){
 		window.location = url;
+	});
+
+	socket.on('soundcloud:control:toggle', function(){
+		$controls.find('.playControl').click();
+	});
+
+	socket.on('soundcloud:control:next', function(){
+		$controls.find('.skipControl__next').click();
+	});
+
+	socket.on('soundcloud:control:previous', function(){
+		$controls.find('.skipControl__previous').click();
 	});
 
 	soundsync.connection = socket;
